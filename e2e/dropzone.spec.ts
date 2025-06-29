5c4e37911361ab373cd41a2b3e2ce2493579f342
Vlashex <rmaynavine@gmail.com>
2025-06-29 20:46:53 +0300

    Починил тест drag&drop

--- e2e/dropzone.spec.ts
+++ e2e/dropzone.spec.ts
@@ -5,11 +5,11 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test('Drop .csv файл успешно загружает его', async ({ page }) => {
+test('Drop .csv файл отображает статус "файл загружен!"', async ({ page }) => {
     await page.goto('/');
 
     const filePath = path.resolve(__dirname, '../tests/fixtures/test.csv');
-    const dropzone = await page.getByTestId('dropzone');
+    const dropzone = page.getByTestId('dropzone');
     const input = await dropzone.locator('input[type="file"]');
 
     await dropzone.dispatchEvent('dragenter');
